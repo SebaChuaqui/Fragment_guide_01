483b7b00f2c0c43a25702fd088164d67f5cf3f53
Seba <pezkaos@gmail.com>
2020-08-12 22:07:09 -0500

    ADD: Dinamico

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,15 +1,42 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
+    private void showFragment() {
+        //Generamos la instancia del fragmento gracias al factory methody
+        FirstFragment firstFragment = FirstFragment.newInstance("", "");
+        //Obtener instancia del FragmentManager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //Obtenemos e instanciamos una transaccion
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        //Añadir el fragmento y lo asociamos al contenedor donde se mostrara
+        fragmentTransaction.add(R.id.content_fragment, firstFragment).addToBackStack(null).commit();
+
+    }
 }
